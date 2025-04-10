518de068fe7dc86c2c8b40f89405769226463655
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-04-10 16:48:52 +0200

    Example freeing memory garbage

---
+++ 205_wrong_freeing_memory_garbage/memory_garbage_free.dpr
@@ -0,0 +1,28 @@
+program memory_garbage_free;
+
+{ THIS IS WRONG APPLICATION,
+  ONLY TO SHOW THE PROBLEM WITH FREEING MEMORY GARBAGE. }
+
+{$APPTYPE CONSOLE}
+
+uses System.SysUtils, Generics.Collections;
+
+procedure Foo;
+var
+  O: TObject; // memory garbage
+begin
+  O := nil;
+  try
+    raise Exception.Create('Error Message');
+    O := TObject.Create;
+    Writeln(O.ClassName);
+  finally
+    FreeAndNil(O);
+  end;
+end;
+
+begin
+  Foo;
+  Readln;
+end.
+
